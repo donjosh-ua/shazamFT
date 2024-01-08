7f0765c7fa233946b61e7009b891881fd0e0a07e
Fab1029 <135774798+Fab1029@users.noreply.github.com>
2024-01-08 12:19:22 -0500

    Update spectrum.py

--- logic/spectrum.py
+++ logic/spectrum.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 from scipy.io.wavfile import read
 
 def plot_spectrogram(audio, Fs, window_length=0.5):
-    audio = audio.reshape(-1)
+    #Determina si el audio esta en uno o dos canales
+    if audio.ndim == 1 :
+        audio = audio.reshape(-1)
+    else :
+         audio = audio[:, 0].ravel()
+        
     window_length_samples = (lambda x: x + x % 2)(int(window_length * Fs))
     amount_to_pad = (lambda x: x - audio.size % x)(window_length_samples)
     song_input = np.pad(audio, (0, amount_to_pad))
