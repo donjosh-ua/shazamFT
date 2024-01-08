df92cb7fcde0709e2c733be03af176c8619c5838
Fab1029 <135774798+Fab1029@users.noreply.github.com>
2024-01-08 12:22:08 -0500

    Update spectrum.py

--- logic/spectrum.py
+++ logic/spectrum.py
@@ -9,7 +9,7 @@ def plot_spectrogram(audio, Fs, window_length=0.5):
         audio = audio.reshape(-1)
     else :
          audio = audio[:, 0].ravel()
-        
+
     window_length_samples = (lambda x: x + x % 2)(int(window_length * Fs))
     amount_to_pad = (lambda x: x - audio.size % x)(window_length_samples)
     song_input = np.pad(audio, (0, amount_to_pad))
@@ -26,7 +26,7 @@ def plot_spectrogram(audio, Fs, window_length=0.5):
 
     # Crear el gráfico del espectrograma
     plt.figure(figsize=(10, 6))
-    plt.imshow(spectrogram.T, aspect='auto', origin='lower', extent=[0, time_index, 0, frequencies[-1]])
+    plt.imshow(spectrogram.T, aspect='auto', origin='lower', extent=[0, len(audio) / Fs, 0, Fs / 2])
     plt.colorbar(label='Magnitud')
     plt.xlabel('Tiempo')
     plt.ylabel('Frecuencia (Hz)')
@@ -34,5 +34,5 @@ def plot_spectrogram(audio, Fs, window_length=0.5):
     plt.show()
 
 
-Fs, audio_input = read('logic/recording.wav')
+Fs, audio_input = read("logic/recording.wav")
 plot_spectrogram(audio_input, Fs)
